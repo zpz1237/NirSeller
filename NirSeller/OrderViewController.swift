d04d834b6aee54cd25f24bc463b5e76622e12e66
NSNirvana <NSNirvana@iCloud.com>
2015-09-09 14:32:21 +0800

    Initial Commit

---
+++ NirSeller/OrderViewController.swift
@@ -0,0 +1,35 @@
+//
+//  OrderViewController.swift
+//  NirSeller
+//
+//  Created by Nirvana on 9/9/15.
+//  Copyright © 2015 NSNirvana. All rights reserved.
+//
+
+import UIKit
+
+class OrderViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
